6a56592ebf7f29b2b78074c7ae486217d68a73e0
Pranav Mehta <pranavm.197@gmail.com>
2023-09-10 10:46:37 +0530

    rolling back refactor for testing

--- tle/cogs/hard75Challenge.py
+++ tle/cogs/hard75Challenge.py
@@ -24,74 +24,74 @@ class Hard75Challenge(commands.Cog):
         self.bot = bot
         self.converter = commands.MemberConverter()
 
-    @commands.command(brief='Hard 75 challenge')
-    @cf_common.user_guard(group='hard75')
-    async def hard75(self,ctx,*args):
-        """
-        The logic behind the bot-
-        -- ;hard75 letsgo
-        Checks in DB if problems have already been assigned for the day 
-            if yes then 
-                if they have been solved then respond accordingly 
-                else respond back with the same problems 
-            else fetch, store (in DB) and return 2 problem (ideally from ACDLadders)
-                1. same level (rounded up) 
-                2. level+ 200 (rounded down)
+    # @commands.command(brief='Hard 75 challenge')
+    # @cf_common.user_guard(group='hard75')
+    # async def hard75(self,ctx,*args):
+    #     """
+    #     The logic behind the bot-
+    #     -- ;hard75 letsgo
+    #     Checks in DB if problems have already been assigned for the day 
+    #         if yes then 
+    #             if they have been solved then respond accordingly 
+    #             else respond back with the same problems 
+    #         else fetch, store (in DB) and return 2 problem (ideally from ACDLadders)
+    #             1. same level (rounded up) 
+    #             2. level+ 200 (rounded down)
         
-        -- ;hard75 completed
-        Checks if problems were assigned to the user on the same day
-            if yes then 
-                if they have solved the problems then
-                    1. update the Hard75 DB-> update streak count properly. 
-                    2. return with his streak count 
-            else respond appropriately
+    #     -- ;hard75 completed
+    #     Checks if problems were assigned to the user on the same day
+    #         if yes then 
+    #             if they have solved the problems then
+    #                 1. update the Hard75 DB-> update streak count properly. 
+    #                 2. return with his streak count 
+    #         else respond appropriately
         
-        -- ;hard75 streak
-            returns the streak of the current user!   
+    #     -- ;hard75 streak
+    #         returns the streak of the current user!   
 
-        -- ;hard75 leaderboard
-            returns the leaderboard!-> which player has the longest streak! 
+    #     -- ;hard75 leaderboard
+    #         returns the leaderboard!-> which player has the longest streak! 
 
 
-            Hard75 DB schema
-            identifier(user)  problem1 , problem2, Streak, lastSolveDate, longesStreak
+    #         Hard75 DB schema
+    #         identifier(user)  problem1 , problem2, Streak, lastSolveDate, longesStreak
                
 
             
-        TBD/future scope:
-            1. cron job to automatically mark completed challengs
-            2. add a certificate for people who complete their 75 days challenge
-        """
-        validSuffixes=["letsgo","completed","streak","leaderboard"]
+    #     TBD/future scope:
+    #         1. cron job to automatically mark completed challengs
+    #         2. add a certificate for people who complete their 75 days challenge
+    #     """
+    #     validSuffixes=["letsgo","completed","streak","leaderboard"]
         
-        if len(args)!=1:
-            await ctx.send('Use the bot properly!')
-            return 
-        elif args[0] not in validSuffixes:
-            await ctx.send('invalid commands used!')
-            return
-        """
-            Use individual functions for each of the above mentioned functionality so as to keep it modular
-        """
-        # await ctx.send("v2")
-        handle, = await cf_common.resolve_handles(ctx, self.converter, ('!' + str(ctx.author),))
-        user = cf_common.user_db.fetch_cf_user(handle)
-        rating = round(user.effective_rating, -2)
-        rating = max(1100, rating)
-        rating = min(3000, rating)
-        submissions = await cf.user.status(handle=handle)
-        solved = {sub.problem.name for sub in submissions}
+    #     if len(args)!=1:
+    #         await ctx.send('Use the bot properly!')
+    #         return 
+    #     elif args[0] not in validSuffixes:
+    #         await ctx.send('invalid commands used!')
+    #         return
+    #     """
+    #         Use individual functions for each of the above mentioned functionality so as to keep it modular
+    #     """
+    #     # await ctx.send("v2")
+    #     handle, = await cf_common.resolve_handles(ctx, self.converter, ('!' + str(ctx.author),))
+    #     user = cf_common.user_db.fetch_cf_user(handle)
+    #     rating = round(user.effective_rating, -2)
+    #     rating = max(1100, rating)
+    #     rating = min(3000, rating)
+    #     submissions = await cf.user.status(handle=handle)
+    #     solved = {sub.problem.name for sub in submissions}
 
 
-        userCommand=args[0]
-        if(userCommand=="letsgo"):
-            await ctx.send('letsgo command would get you the problems once coded!')
-        elif(userCommand=="completed"):
-            await ctx.send('completed command would get you your status once coded')
-        elif(userCommand=="streak"):
-            await ctx.send('streak command would get you the sreak once coded!')
-        elif(userCommand=="leaderboard"):
-            await ctx.send('leaderboard command would get you the leaderboard once coded!')
+    #     userCommand=args[0]
+    #     if(userCommand=="letsgo"):
+    #         await ctx.send('letsgo command would get you the problems once coded!')
+    #     elif(userCommand=="completed"):
+    #         await ctx.send('completed command would get you your status once coded')
+    #     elif(userCommand=="streak"):
+    #         await ctx.send('streak command would get you the sreak once coded!')
+    #     elif(userCommand=="leaderboard"):
+    #         await ctx.send('leaderboard command would get you the leaderboard once coded!')
 
 async def setup(bot):
     await bot.add_cog(Hard75Challenge(bot))
