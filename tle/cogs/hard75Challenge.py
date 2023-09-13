ae81a7207b2a93a91365f420846c6d7924fd8e6f
Denjell <73618585+Denjell@users.noreply.github.com>
2023-09-13 22:29:27 +0200

    Leaderboard 2.0

--- tle/cogs/hard75Challenge.py
+++ tle/cogs/hard75Challenge.py
@@ -141,8 +141,7 @@ class Hard75Challenge(commands.Cog):
         def make_page(chunk, page_num):
             style = table.Style('{:>}  {:<}  {:>}  {:>}')
             t = table.Table(style)
-            t += table.Header('', '', 'Longest', 'Current')
-            t += table.Header('#', 'Name', 'streak', 'streak')
+            t += table.Header('#', 'Name', 'Max', 'Curr')
             t += table.Line()
             for index, (member, longestStreak, currentStreak) in enumerate(chunk):
                 lstreakstr = f'{longestStreak}' 
