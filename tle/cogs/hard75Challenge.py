b4ae38f292ff4949795bacd92eb32408137d031c
Denjell <73618585+Denjell@users.noreply.github.com>
2023-09-18 22:44:47 +0200

    help changes

--- tle/cogs/hard75Challenge.py
+++ tle/cogs/hard75Challenge.py
@@ -72,7 +72,7 @@ class Hard75Challenge(commands.Cog):
     def _generateStreakEmbed(self, handle, current_streak, longest_streak, last_updated):
         embed = discord.Embed(title=f'{handle}s Hard75 grind!')
         today=datetime.datetime.utcnow().strftime('%Y-%m-%d')
-        last_updated_str = "never" if last_updated==0 else last_updated
+        last_updated_str = "never" if last_updated=='0' else last_updated
         last_updated_str = "today" if last_updated==today else last_updated_str
         embed.add_field(name='current streak', value=current_streak)
         embed.add_field(name='longest streak', value=longest_streak)
